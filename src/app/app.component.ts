7afa7f4b944adec6fcecc3ec146697be7471287e
Selvakumar2000 <skselva312000@gmail.com>
2021-10-27 20:03:10 +0530

    "AccordianPanel Developed Successfully"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,119 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularMaterialUsedApp';
+ 
+  //badge
+  notifications=0;
+
+  //spinner
+  showSpinner = false;
+
+  loadData()
+  {
+    this.showSpinner=true;
+    setTimeout(() => {
+      this.showSpinner = false;
+    }, 5000);
+  }
+
+  //sidenave
+  opened = false;
+
+  log(state: any)
+  {
+    console.log(state);
+  }
+
+  //accordian
+
+  // openMep1:any = false;
+  //  panelFocus = { };
+  
+  // openMep(isOpen:any)
+  // {
+  //   this.openMep1 = isOpen;
+  //   this.panelFocus = {
+  //     "box" : this.openMep1 
+  //   }
+  // }
+
+  // closeMep(isOpen:any)
+  // {
+  //   this.openMep1 = isOpen;
+  //   this.panelFocus = {
+  //     "box" : this.openMep1 
+  //   }
+  // }
+
+  //accordian pattern implementation
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+    
+  }
+
+  
+
+    //radio button
+    favoriteColor: string = 'blue';
+    colors: string[] = ['blue', 'gray', 'tblue'];
+    constructor() { }
+  
+    ngOnInit()
+    {
+     
+    }
+  //Reference --> https://stackoverflow.com/questions/54554380/checked-attribute-not-working-in-mat-radio-button
+    radioChange(e:any)
+    {
+      console.log(this.favoriteColor);  
+      this.setVisible(true);
+    }
+
+    closePan(state:any)
+    {
+      console.log(state);
+     
+    }
+
+  panelFocus = { };
+  setVisible(state:boolean)
+  {
+   
+
+    if(this.favoriteColor === 'blue')
+    {
+      this.panelFocus = {
+        "box" : state
+      }
+
+    }
+
+    if(this.favoriteColor === 'gray')
+    {
+      this.panelFocus = {
+        "box1" : state
+      }
+
+    }
+
+    if(this.favoriteColor === 'tblue')
+    {
+      this.panelFocus = {
+        "box2" : state
+      }
+
+      
+    }
+    
+  }
+
+
 }
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+
+
+import { MatSliderModule } from '@angular/material/slider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+const MaterialComponents = [
+  MatSliderModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatMenuModule,
+  MatListModule,
+  MatDividerModule,
+  MatGridListModule,
+  MatRadioModule,
+  MatCheckboxModule,
+]
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+export class MaterialModule { }
